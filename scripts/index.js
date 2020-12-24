92ee97be82d85f3f249e70443186155fc693d5fb
Daguinson Fleurantin <dague04@gmail.com>
2020-12-23 21:19:43 -0500

    update index js

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,19 @@
 let modal = document.querySelector(".modal");
-let overlay = document.querySelector(".modal__overlay");
-let btncloseModal = document.querySelector(".modal__closebtn");
+let btncloseModal = modal.querySelector(".modal__closebtn");
 let btnEditProfile = document.querySelector(".profile__edit-button");
 let profileModalForm = document.querySelector(".modal__profile-form");
 let username = document.querySelector(".profile__name");
 let profession = document.querySelector(".profile__profession");
-let modalUsername = document.querySelector(".modal__input-name");
-let modalProfession = document.querySelector(".modal__input-profession");
+let modalUsername = profileModalForm.querySelector(".modal__input-name");
+let modalProfession = profileModalForm.querySelector(
+  ".modal__input-profession"
+);
 let profileInfo = document.querySelector(".profile__info");
 
 const openEditProfile = function () {
   modal.classList.remove("hidden");
+  modalUsername.value = username.textContent;
+  modalProfession.value = profession.textContent;
 };
 
 const closeModal = function () {
